965239576025e066511a78525a48001b1457c7ba
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-06-26 10:59:50 +0300

    Work in progress

--- src/index.js
+++ src/index.js
@@ -1,45 +1,74 @@
-'use strict';
+"use strict";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const context = github.context;
+const exec = require("@actions/exec");
+const XRegExp = require("xregexp");
+const fs = require("fs");
+const util = require("util");
 
 const SEMANTIC_VERSION_REGEX =
-    XRegExp('/^(?P<major>0|[1-9]\\d*)\\.(?P<minor>0|[1-9]\\d*)\\.(?P<patch>0|[1-9]\\d*)(?:-(?P<prerelease>(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$/')
+    XRegExp("/^(?P<major>0|[1-9]\\d*)\\.(?P<minor>0|[1-9]\\d*)\\.(?P<patch>0|[1-9]\\d*)(?:-(?P<prerelease>(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$/");
 
-const core = require('@actions/core');
-const github = require('@actions/github');
-const context = github.context;
-const exec = require('@actions/exec');
+function getFileContents(file) {
+    const readPromise = util.promisify(fs.readFile)
+    return readPromise(file, {"encoding": "utf-8"});
+}
 
-async function getFileVersion() {
-    let versionFile = core.getInput('version-file', {required: true});
-    let extractionPattern = core.getInput('version-file-extraction-pattern', {required: true});
-    let myOutput = '';
-    let myError = '';
-    let options = {};
-    options.listeners = {
-        stdout: (data) => {
-            myOutput += data.toString();
-        },
-        stderr: (data) => {
-            myError += data.toString();
+function getFileVersion(versionFile, extractionPattern) {
+    const fileContent = getFileContents(versionFile);
+
+    return fileContent.then((content) => {
+        const pattern = XRegExp(extractionPattern, ["g"]);
+        const match = XRegExp.exec(content, pattern);
+
+        if (match) {
+            if (match.length > 1) {
+                return match[1];
+            } else {
+                return match[0];
+            }
         }
-    };
-    await exec.exec('cat', [versionFile], options)
+        return null;
+    });
+}
 
+function parseVersion(version) {
+    const result = XRegExp.exec(SEMANTIC_VERSION_REGEX, ["g"]);
+    if(!result) {
+        throw new Error("Unable to parse version: " + version);
+    }
+    return result[1];
 }
 
 async function run() {
-    let versionSource = core.getInput('version-source');
+    const versionSource = core.getInput("version-source");
 
     let version;
     switch (versionSource) {
-        case 'file':
-            version = await getFileVersion();
+        case "file":
+            version = await getFileVersion(core.getInput("version-file", {required: true}),
+                core.getInput("version-file-extraction-pattern", {required: true}));
             break;
-        case 'variable':
-            version = core.getInput('version');
+        case "variable":
+            version = core.getInput("version");
             break;
     }
+
+    if (!version) {
+        core.setFailed("Unable to get version: null");
+        return;
+    }
+
+    const parsedVersion = parseVersion(version)
+
 }
 
 run().catch(error => {
     core.setFailed(error.message);
 });
+
+
+module.exports = {"getFileVersion": getFileVersion,
+"parseVersion": parseVersion};
