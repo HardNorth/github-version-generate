2e2284889af781e58d0b1e9f3277b01f22946553
Vadzim Hushchanskou <vadzim.hushchanskou@gmail.com>
2020-06-26 10:59:50 +0300

    Work in progress

---
+++ tests/index.test.js
@@ -0,0 +1,26 @@
+const index = require("../src/index");
+
+// Version file read
+test("Test simple property file version extraction by match", async () => {
+    const result = await index.getFileVersion("tests/resources/simple_gradle.properties", "(?<=version=).+");
+    expect(result).toBe("5.0.3-SNAPSHOT");
+});
+
+test("Test simple property file version extraction by group", async () => {
+    const result = await index.getFileVersion("tests/resources/simple_package.json", "\"version\":\\s*\"([^\"]+)\"");
+    expect(result).toBe("1.0.0");
+});
+
+
+test("Test getFileVersion function throws exception on not existing file", async () => {
+    const file = "resources/no_such_file.txt";
+    await expect(index.getFileVersion(file, "(?<=version=).+")).rejects.toMatchObject({
+        code: "ENOENT"
+    });
+});
+
+// Version parse
+test("Test simple version parse", () => {
+    const result = index.parseVersion("5.0.3-SNAPSHOT");
+    expect(result).toBe("5.0.3-SNAPSHOT");
+});
