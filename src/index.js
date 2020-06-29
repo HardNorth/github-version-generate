867c57a59efce6db831134ac675b6db58ea78bed
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-06-29 12:29:37 +0300

    Work in progress

--- src/index.js
+++ src/index.js
@@ -11,17 +11,52 @@ const util = require("util");
 const SEMANTIC_VERSION_REGEX =
     XRegExp("^(?P<major>0|[1-9]\\d*)\\.(?P<minor>0|[1-9]\\d*)\\.(?P<patch>0|[1-9]\\d*)(?:-(?P<prerelease>(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$");
 
+const PRERELEASE_SEPARATOR = "-";
+const SNAPSHOT = "SNAPSHOT";
+const SNAPSHOT_SUFFIX = PRERELEASE_SEPARATOR + SNAPSHOT;
+
+
+function Properties(propertySource) {
+    // Version source
+    this.versionSource = propertySource.getInput("version-source", {required: true});
+    let fileRequired = false;
+    if (this.versionSource === "file") {
+        fileRequired = true;
+    }
+    this.versionFile = propertySource.getInput("version-file", {required: fileRequired});
+    this.versionFileExtractPattern = propertySource.getInput("version-file-extraction-pattern", {required: fileRequired});
+    this.version = propertySource.getInput("version", {required: !fileRequired});
+
+    // Next version put build metadata
+    this.nextMetadata = propertySource.getInput("next-version-put-build-metadata", {required: true}) === "true";
+
+    // Release version
+    this.releaseCutSnapshot = propertySource.getInput("release-version-cut-snapshot", {required: true}) === "true";
+    this.releaseCutMetadata = propertySource.getInput("release-version-cut-snapshot", {required: true}) === "true";
+    this.releaseGenerateMetadata = propertySource.getInput("release-version-generate-build-metadata", {required: true}) === "true";
+    this.releaseMetadataPattern = propertySource.getInput("release-version-build-metadata-pattern", {required: this.releaseGenerateMetadata || this.nextMetadata});
+    this.releaseMetadataTime = propertySource.getInput("release-version-build-metadata-datetime");
+
+    // Next version
+    this.nextIncrementMajor = propertySource.getInput("next-version-increment-major", {required: true}) === "true";
+    this.nextIncrementMinor = propertySource.getInput("next-version-increment-minor", {required: true}) === "true";
+    this.nextIncrementPatch = propertySource.getInput("next-version-increment-patch", {required: true}) === "true";
+    this.nextIncrementPrerelease = propertySource.getInput("next-version-increment-prerelease", {required: true}) === "true";
+
+
+}
+
 function Version(parseResultArray) {
     this.raw = parseResultArray.input;
     this.major = parseInt(parseResultArray.major);
     this.minor = parseInt(parseResultArray.minor);
     this.patch = parseInt(parseResultArray.patch);
-    if (typeof parseResultArray.prerelease !== 'undefined') {
+    if (typeof parseResultArray.prerelease !== "undefined") {
         this.prerelease = parseResultArray.prerelease;
     } else {
         this.prerelease = null;
     }
-    if (typeof parseResultArray.buildmetadata !== 'undefined') {
+    if (typeof parseResultArray.buildmetadata !== "undefined") {
         this.buildMetadata = parseResultArray.buildmetadata;
     } else {
         this.buildMetadata = null;
@@ -71,35 +106,70 @@ function parseVersion(version) {
     return new Version(result);
 }
 
+function generateMetadata(version, pattern) {
+
+}
+
+function generateReleaseVersion(currentVersion, properties) {
+    const releaseVersion = new Version(currentVersion);
+
+    if (properties.releaseCutSnapshot) {
+        let prerelease = releaseVersion.prerelease;
+        if (prerelease === SNAPSHOT) {
+            prerelease = null;
+        } else if (prerelease.endsWith(SNAPSHOT_SUFFIX)) {
+            prerelease = prerelease.substring(0, prerelease.length - SNAPSHOT_SUFFIX.length);
+        }
+        releaseVersion.prerelease = prerelease;
+    }
+
+    if (properties.releaseCutMetadata) {
+        releaseVersion.buildMetadata = null;
+    }
+
+    if (properties.releaseGenerateMetadata) {
+        generateMetadata(releaseVersion, properties.releaseMetadataPattern);
+    }
+    return releaseVersion;
+}
+
 async function run() {
-    const versionSource = core.getInput("version-source");
+    const properties = new Properties(core);
 
     let version;
-    switch (versionSource) {
+    switch (properties.versionSource) {
         case "file":
-            version = await getFileVersion(core.getInput("version-file", {required: true}),
-                core.getInput("version-file-extraction-pattern", {required: true}));
+            version = await getFileVersion(properties.versionFile, properties.versionFileExtractPattern);
             break;
         case "variable":
-            version = core.getInput("version");
+            version = properties.version;
             break;
     }
 
+    // Check version extracted
     if (!version) {
         core.setFailed("Unable to get version: null");
         return;
     }
 
+    // Parse and set 'CURRENT_VERSION' outputs
     const parsedVersion = parseVersion(version);
+    const currentVersionStr = parsedVersion.toString();
+    core.info("Got version extracted: " + currentVersionStr);
+    core.exportVariable("CURRENT_VERSION", currentVersionStr);
+    core.setOutput("CURRENT_VERSION", currentVersionStr);
 
+    const releaseVersion = generateReleaseVersion(parsedVersion, properties);
 }
 
 run().catch(error => {
     core.setFailed(error.message);
 });
 
-
 module.exports = {
+    "Properties": Properties,
+    "Version": Version,
     "getFileVersion": getFileVersion,
-    "parseVersion": parseVersion
+    "parseVersion": parseVersion,
+    "generateReleaseVersion": generateReleaseVersion
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,15 +2,16 @@ const each = require("jest-each").default;
 
 const index = require("../src/index");
 
-describe("Test version file read successful", () =>{
-   each([
-       ["tests/resources/simple_gradle.properties", "(?<=version=).+", "5.0.3-SNAPSHOT"],
-       ["tests/resources/more_complex_gradle.properties", "(?<=version=).+", "5.0.0-BETA-16-SNAPSHOT"],
-       ["tests/resources/simple_package.json", "\"version\":\\s*\"([^\"]+)\"", "1.0.0"],
-   ]).it("When version file is '%s'; patters is '%s'", async (file, pattern, expectedVersion) => {
-       const result = await index.getFileVersion(file, pattern);
-       expect(result).toBe(expectedVersion);
-   })
+describe("Test version file read successful", () => {
+    each([
+        ["tests/resources/simple_gradle.properties", "(?<=version=).+", "5.0.3-SNAPSHOT"],
+        ["tests/resources/more_complex_gradle.properties", "(?<=version=).+", "5.0.0-BETA-16-SNAPSHOT"],
+        ["tests/resources/simple_package.json", "\"version\":\\s*\"([^\"]+)\"", "1.0.0"],
+        ["tests/resources/version.txt", ".+", "0.0.1-SNAPSHOT"]
+    ]).it("When version file is '%s'; patters is '%s'", async (file, pattern, expectedVersion) => {
+        const result = await index.getFileVersion(file, pattern);
+        expect(result).toBe(expectedVersion);
+    });
 });
 
 test("Test getFileVersion function throws exception on not existing file", async () => {
@@ -35,6 +36,24 @@ describe("Test version parse successful", () => {
         expect(result.patch).toBe(patch);
         expect(result.prerelease).toBe(prerelease);
         expect(result.buildMetadata).toBe(buildMetadata);
-    })
+    });
 });
 
+describe("Test toString", () => {
+        each([
+            ["6.5.1-SNAPSHOT", "6", "5", "1", "SNAPSHOT", undefined],
+            ["1.2.3-SNAPSHOT+build.2017-02-03.3e1f4d", "1", "2", "3", "SNAPSHOT", "build.2017-02-03.3e1f4d"],
+            ["100000.2341234123.12341235+build.2017-02-03.3e1f4d", 100000, 2341234123, 12341235, undefined, "build.2017-02-03.3e1f4d"],
+            ["3.2.1", "3", "2", "1", undefined, undefined]
+        ]).it("When version is %s", (expected, major, minor, patch, prerelease, buildMetadata) => {
+            const result = new index.Version({
+                "major": major,
+                "minor": minor,
+                "patch": patch,
+                "prerelease": prerelease,
+                "buildmetadata": buildMetadata
+            }).toString();
+            expect(result).toBe(expected);
+        });
+    }
+);
