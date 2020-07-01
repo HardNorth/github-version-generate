100c7af05b72a239d6a17027756ff38acb334b0a
Vadzim Hushchanskou <vadzim.hushchanskou@gmail.com>
2020-07-01 12:27:13 +0300

    Work in progress

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,5 @@
+process.env["GITHUB_SHA"] = "8278cdafa198a07b718945a97602bbffa5511f2b";
+
 const each = require("jest-each").default;
 
 const index = require("../src/index");
@@ -28,14 +30,14 @@ describe("Test version parse successful", () => {
         ["5.0.0-BETA-16-SNAPSHOT", 5, 0, 0, "BETA-16-SNAPSHOT", null],
         ["5.0.0-BETA-16-SNAPSHOT+build.2017-03-15.3ecfad", 5, 0, 0, "BETA-16-SNAPSHOT", "build.2017-03-15.3ecfad"],
         ["5.0.0-TESTNG6-BETA-16-SNAPSHOT+build.2017-03-15.3ecfad", 5, 0, 0, "TESTNG6-BETA-16-SNAPSHOT", "build.2017-03-15.3ecfad"]
-    ]).it("When versions is %s", (version, major, minor, patch, prerelease, buildMetadata) => {
+    ]).it("When versions is %s", (version, major, minor, patch, prerelease, buildmetadata) => {
         const result = index.parseVersion(version);
         expect(result.raw).toBe(version);
         expect(result.major).toBe(major);
         expect(result.minor).toBe(minor);
         expect(result.patch).toBe(patch);
         expect(result.prerelease).toBe(prerelease);
-        expect(result.buildMetadata).toBe(buildMetadata);
+        expect(result.buildmetadata).toBe(buildmetadata);
     });
 });
 
@@ -45,13 +47,13 @@ describe("Test toString", () => {
             ["1.2.3-SNAPSHOT+build.2017-02-03.3e1f4d", "1", "2", "3", "SNAPSHOT", "build.2017-02-03.3e1f4d"],
             ["100000.2341234123.12341235+build.2017-02-03.3e1f4d", 100000, 2341234123, 12341235, undefined, "build.2017-02-03.3e1f4d"],
             ["3.2.1", "3", "2", "1", undefined, undefined]
-        ]).it("When version is %s", (expected, major, minor, patch, prerelease, buildMetadata) => {
+        ]).it("When version is %s", (expected, major, minor, patch, prerelease, buildmetadata) => {
             const result = new index.Version({
                 "major": major,
                 "minor": minor,
                 "patch": patch,
                 "prerelease": prerelease,
-                "buildmetadata": buildMetadata
+                "buildmetadata": buildmetadata
             }).toString();
             expect(result).toBe(expected);
         });
@@ -122,14 +124,98 @@ test("Test properties fail if version-source is 'variable' and no 'version' spec
 
 // Metadata generation
 
+const METADATA_DEFAULT_MODEL = {
+    date: new Date(2020, 2, 2, 17, 33, 3),
+    hash: "622161f9e2993288c026f9c8eb71a0659ed933ee"
+};
+
 const METADATA_TEST_CASES = [
-    "build.{date}.{hash}",
-    "build.{date[YYYY-MM-dd]}.{hash}",
-    "build.{date[YYYY-MM-dd]}.{hash[0,6]}",
-    "build.{date[ YYYY-MM-dd ]}.{hash[ 0, 6 ]}",
-    "{date}-{hash}-something",
-    "build-{date}-{hash}",
-    "build.{date}.hash.{hash}",
-    "{date}.{hash[0,6]}",
-    "build.2342234."
+    ["build.{date}.{hash}", "build.2017-03-02.622161f9", {
+        ...METADATA_DEFAULT_MODEL,
+        date: new Date(2017, 2, 2, 17, 33, 3)
+    }],
+    ["build.{date[YYYY-MM-dd-HH]}.{hash}", "build.2020-03-Mo-17.622161f9", METADATA_DEFAULT_MODEL],
+    ["build.{date[YYYY.MM]}.{hash[0,10]}", "build.2020.08.7c8146ed9b", {
+        date: new Date(2020, 7, 2, 17, 33, 3),
+        hash: "7c8146ed9b99476e4e53e97f3ebd9a4b24a69c3d"
+    }],
+    ["build.{date[ YYYY-MM-DD ]}.{hash[ 0, 10 ]}", "build.2020-03-02.622161f9e2", METADATA_DEFAULT_MODEL],
+    ["{date}-{hash}-something", "2020-03-02-7c8146ed-something", {
+        ...METADATA_DEFAULT_MODEL,
+        hash: "7c8146ed9b99476e4e53e97f3ebd9a4b24a69c3d"
+    }],
+    ["build-{date}-{hash}", "build-2020-03-02-622161f9", METADATA_DEFAULT_MODEL],
+    ["build.{date}.hash.{hash}", "build.2020-03-02.hash.622161f9", METADATA_DEFAULT_MODEL],
+    ["{date}.{hash[0,6]}", "2020-03-02.622161", METADATA_DEFAULT_MODEL],
+    ["build.2342234.", "build.2342234.", METADATA_DEFAULT_MODEL],
+    ["{date}.{hash[0,10000000000000000000]}", "2020-03-02.622161f9e2993288c026f9c8eb71a0659ed933ee", METADATA_DEFAULT_MODEL],
+    ["{date}.{hash[-1000,6]}", "2020-03-02.622161", METADATA_DEFAULT_MODEL],
+    ["{date}.{hash[10000000000000000000, 0]}", "2020-03-02.622161f9e2993288c026f9c8eb71a0659ed933ee", METADATA_DEFAULT_MODEL]
 ];
+
+describe("Test metadata generation with different patterns", () => {
+    each(METADATA_TEST_CASES).it("When pattern is '%s'", (pattern, expected, model) => {
+        const result = index.generateMetadata(pattern, model);
+        expect(result).toBe(expected);
+    });
+});
+
+// Release version generation
+
+const DATE = new Date(Date.now());
+const PAST_DATE = new Date(2017, 2, 4, 17, 33, 53);
+const DATE_FORMAT = new Intl.NumberFormat("en-US", {"minimumIntegerDigits": 2});
+
+const CURRENT_VERSION = new index.Version({
+    input: "1.2.3-BETA-7-SNAPSHOT+build.2017-02-03.3e1f4d",
+    major: "1",
+    minor: "2",
+    patch: "3",
+    prerelease: "BETA-7-SNAPSHOT",
+    buildmetadata: "build.2017-02-03.3e1f4d"
+});
+
+const INCORRECT_SNAPSHOT_VERSION = new index.Version({
+    input: "1.2.3-BETA-SNAPSHOT-7+build.2017-02-03.3e1f4d",
+    major: "1",
+    minor: "2",
+    patch: "3",
+    prerelease: "BETA-SNAPSHOT-7",
+    buildmetadata: "build.2017-02-03.3e1f4d"
+});
+
+const RELEASE_VERSION_TEST_CASES = [
+    [{}, CURRENT_VERSION, "1.2.3-BETA-7"],
+    [{"INPUT_release-version-cut-build-metadata": "false"}, CURRENT_VERSION, "1.2.3-BETA-7+build.2017-02-03.3e1f4d"],
+    [{
+        "INPUT_release-version-cut-snapshot": "false",
+        "INPUT_release-version-cut-build-metadata": "false"
+    }, CURRENT_VERSION, "1.2.3-BETA-7-SNAPSHOT+build.2017-02-03.3e1f4d"],
+    [{"INPUT_release-version-cut-snapshot": "false"}, CURRENT_VERSION, "1.2.3-BETA-7-SNAPSHOT"],
+    [{}, INCORRECT_SNAPSHOT_VERSION, "1.2.3-BETA-SNAPSHOT-7"],
+    [{
+        "INPUT_release-version-generate-build-metadata": "true"
+    }, CURRENT_VERSION, "1.2.3-BETA-7+build." + DATE.getUTCFullYear() + "-" + DATE_FORMAT.format(DATE.getUTCMonth() + 1) + "-" + DATE_FORMAT.format(DATE.getUTCDate()) + ".8278cdaf"],
+    [{
+        "INPUT_release-version-generate-build-metadata": "true",
+        "INPUT_release-version-build-metadata-datetime": PAST_DATE.toISOString()
+    }, CURRENT_VERSION, "1.2.3-BETA-7+build." + PAST_DATE.getUTCFullYear() + "-" + DATE_FORMAT.format(PAST_DATE.getUTCMonth() + 1) + "-" + DATE_FORMAT.format(PAST_DATE.getUTCDate()) + ".8278cdaf"],
+    [{
+        "INPUT_release-version-generate-build-metadata": "true",
+        "INPUT_release-version-build-metadata-datetime": PAST_DATE.getFullYear() + "-" + (PAST_DATE.getMonth() + 1) + "-" + PAST_DATE.getDate()
+    }, CURRENT_VERSION, "1.2.3-BETA-7+build." + PAST_DATE.getUTCFullYear() + "-" + DATE_FORMAT.format(PAST_DATE.getUTCMonth() + 1) + "-" + DATE_FORMAT.format(PAST_DATE.getUTCDate()) + ".8278cdaf"]
+];
+
+describe("Test release version generation with different properties", () => {
+    each(RELEASE_VERSION_TEST_CASES).it("When inputs are '%s'; and version is: '%s'; expected is '%s'", (inputs, currentVersion, expected) => {
+        for (const key in MINIMAL_CORRECT_INPUTS) {
+            process.env[key] = MINIMAL_CORRECT_INPUTS[key];
+        }
+        for (const key in inputs) {
+            process.env[key] = inputs[key];
+        }
+
+        const result = index.generateReleaseVersion(currentVersion, new index.Properties()).toString();
+        expect(result).toBe(expected);
+    });
+});
